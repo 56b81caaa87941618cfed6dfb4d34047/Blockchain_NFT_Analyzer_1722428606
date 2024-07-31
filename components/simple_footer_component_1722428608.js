dac4096c3e59c1d6c4eea5453df83dc584c6e5d4
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-31 12:23:36 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722428608.js
+++ components/simple_footer_component_1722428608.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722428608", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            NFTalyst - Blockchain NFT Analytics
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Gain powerful insights into the latest NFT trends and collections across multiple blockchain networks. Stay ahead of the curve with real-time analytics and data visualization tools tailored for NFT enthusiasts and investors.
                     </div>
                 </div>
             </div>
